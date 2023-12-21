b396547a2c257f1ea58f5539f8a9f133e33951ad
FREDRICK KYEKI <fredrickisaac142@gmail>
2023-12-21 17:09:48 +0300

    [fix]: print stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,43 +20,39 @@ the same output as this on
 """
 import sys
 
-count = 0
-total_size = 0
-status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
 
-
-def print_metrics():
-    """prints the statistics from the beginning"""
-
-    print("File size: {}".format(total_size))
-
-    for key, value in sorted(status_code.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+words = []
+size_list = []
+status_dict = {'200': 0, '301': 0, '400': 0,
+               '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+sum_size = 0
 
 try:
     for line in sys.stdin:
-
-        try:
-            code = line.split()[-2]
-            if code in status_code.keys():
-                status_code[code] += 1
-        except BaseException as _:
-            pass
-
-        try:
-            size = line.split()[-1]
-            total_size += int(size)
-        except BaseException as _:
-            pass
-
-        count += 1
-        if (count % 10 == 0):
-            print_metrics()
-
-    print_metrics()
-
+        words.append(line)
+        if len(words) % 10 == 0:
+            for i in range(len(words)):
+                size_file = words[i]
+                file_split = size_file.split(' ')
+                file_size = file_split[-1]
+                status_code = file_split[-2]
+                if status_code in status_dict:
+                    status_dict[status_code] += 1
+                size_list.append(file_size)
+            for sizes in size_list:
+                sum_size = sum_size + int(sizes)
+            print("File size: {}".format(sum_size))
+            for k, v in status_dict.items():
+                if status_dict[k] != 0:
+                    print("{}: {}".format(k, status_dict[k]))
+            status_dict = {'200': 0, '301': 0, '400': 0,
+                           '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 except KeyboardInterrupt:
-    print_metrics()
-    raise
+    pass
+finally:
+    for sizes in size_list:
+        sum_size = sum_size + int(sizes)
+    print("File size: {}".format(sum_size))
+    for k, v in status_dict.items():
+        if status_dict[k] != 0:
+            print("{}: {}".format(k, status_dict[k]))
